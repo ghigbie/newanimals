eef1c1fb701a68d57f1fc19d3592380ad70fd744
Higbie, George (Y8819) <george.higbie@usaa.com>
2020-02-28 10:58:05 -0700

    Initial commit

---
+++ app/src/main/java/com/trainingtrack/newanimals/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.trainingtrack.newanimals.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trainingtrack.newanimals.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
